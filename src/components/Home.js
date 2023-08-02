5472f60ec44c3812363a147950104edcf154ae84
styp3r <stypertypes@gmail.com>
2023-08-02 23:49:10 +0530

    Adds button titles and status with date and supervising indicators

--- src/components/Home.js
+++ src/components/Home.js
@@ -38,20 +38,47 @@ const Home = () => {
                         <p>2/08/2023 -notif-</p>
                     </div>
                     <div className="body-side-container">
-                        <div className = "survey-title">
-                        <p>Restaurant Survey</p>
-                        <p>Daily checks and visit updates</p>
+                        <div className="survey-title">
+                            <p>Restaurant Survey</p>
+                            <p>Daily checks and visit updates</p>
                         </div>
                         <div className="survey-section-container">
-                            <div className="btm-survey"></div>
-                            <div className="ec2-survey"></div>
-                            <div className="ddk-survey"></div>
-                            <div className="mll-survey"></div>
-                            <div className="vtr-survey"></div>
-                            <div className="jbn-survey"></div>
-                            <div className="orr-survey"></div>
-                            <div className="knr-survey"></div>
-                            <div className="hmu-survey"></div>
+                            <div className="btm-survey">
+                                <p className="survey-shop-title">BTM</p>
+                                <p className="survey-shop-date-status">last visited - 02/08/2023</p>
+                            </div>
+                            <div className="ec2-survey">
+                                <p className="survey-shop-title">EC2</p>
+                                <p className="survey-shop-date-status">supervising</p>
+                            </div>
+                            <div className="ddk-survey">
+                                <p className="survey-shop-title">DDK</p>
+                                <p className="survey-shop-date-status">supervising</p>
+                            </div>
+                            <div className="mll-survey">
+                                <p className="survey-shop-title">MLL</p>
+                                <p className="survey-shop-date-status">supervising</p>
+                            </div>
+                            <div className="vtr-survey">
+                                <p className="survey-shop-title">VTR</p>
+                                <p className="survey-shop-date-status">supervising</p>
+                            </div>
+                            <div className="jbn-survey">
+                                <p className="survey-shop-title">JBN</p>
+                                <p className="survey-shop-date-status">supervising</p>
+                            </div>
+                            <div className="orr-survey">
+                                <p className="survey-shop-title">ORR</p>
+                                <p className="survey-shop-date-status">supervising</p>
+                            </div>
+                            <div className="knr-survey">
+                                <p className="survey-shop-title">KNR</p>
+                                <p className="survey-shop-date-status">supervising</p>
+                            </div>
+                            <div className="hmu-survey">
+                                <p className="survey-shop-title">HMU</p>
+                                <p className="survey-shop-date-status">supervising</p>
+                            </div>
                         </div>
                     </div>
                 </div>
