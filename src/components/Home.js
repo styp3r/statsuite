b53cbbc551f009370679c583630768ca26b00386
styp3r <stypertypes@gmail.com>
2023-08-02 23:15:22 +0530

    Creates basic layout for the body of the home page with relevant elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,44 @@
 import Navbar from './Navbar'
-const Home = () =>{
+const Home = () => {
     return (
-        <div>
-            <Navbar />
-            <p>Home</p>
+        <div id="home">
+            <div className="home-container">
+                <div className="main-container">
+                    <div className="top-section">
+                        <div className="greeting-section">
+                            <p>Dashboard</p>
+                            <p>Hello Navjyot! Here are your updates for today</p>
+                        </div>
+                    </div>
+                    <div className="body-section">
+                        <div className="body-container">
+                            <div className="stat-box-container">
+                                <div style = {{backgroundColor: '#957fef'}} className = "stat-box"></div>
+                                <div style = {{backgroundColor: '#2a2b2e'}} className = "stat-box"></div>
+                                <div style = {{backgroundColor: '#dcdcdc'}} className = "stat-box"></div>
+                            </div>
+                            <div className="cash-status-box-container">
+                                <div style = {{backgroundColor: '#ffffff', border: 'solid 3px #dcdcdc'}} className = "cash-status-box"></div>
+                                <div style = {{backgroundColor: '#ffffff', border: 'solid 3px #dcdcdc'}} className = "cash-status-box"></div>
+                            </div>
+                            <div className = "revOverview-container">
+                                <div className = "revOverview-chart-container"></div>
+                            </div>
+                            <div className = "outletWise-overview-container">
+                            <div className = "outletwise-overview-chart-container"></div>
+                            </div>
+                            <div className = "yearlyRev-overview-container">
+                            <div className = "yearlyRev-overview-chart-container"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="side-container">
+                    <div className="dateNotif-section">
+                        <p>2/08/2023 -notif-</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div id="navbar">
-            <Link to="/">Home</Link>
-            <Link to="/sales">Sales</Link>
+            <Link className = "link" to="/">Home</Link>
+            <Link className = "link" to="/sales">Sales</Link>
+            <Link className = "link" to="/sales">Purchases</Link>
+            <Link className = "link" to="/sales">Vendors</Link>
         </div>
     );
 }
