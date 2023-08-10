cd792f30032e729443afd1582a6149ae52283162
styp3r <stypertypes@gmail.com>
2023-08-10 16:17:39 +0530

    Fixes display layout bug. Now displays sale figures under relevant shop boxes

--- src/components/SaleFiguresBox.js
+++ src/components/SaleFiguresBox.js
@@ -1,14 +1,22 @@
 const SaleFiguresBox = (props) => {
     return (
         <div id="sale-figures-box">
-            <div className = "sale-figures-box-id">
-                <p className="sale-figures-box-shopname">{props.shop_name}</p>
+            <div className="sale-figures-box-id">
                 <p className="sale-figures-box-date">{props.date}</p>
             </div>
             <div className="sale-figures-container">
-                <p className="sale-cash-sale">{props.cash_sale}</p>
-                <p className="sale-card-sale">{props.card_sale}</p>
-                <p className="sale-upi-sale">{props.upi_sale}</p>
+                <div className="single-sale-figure-container">
+                    <div className="left-sale-figures">
+                        <p className="sale-cash-sale">Cash Sale - {props.cash_sale}</p>
+                        <p className="sale-card-sale">Card Sale - {props.card_sale}</p>
+                        <p className="sale-upi-sale">UPI Sale - {props.upi_sale}</p>
+                    </div>
+                    <div className="right-sale-figures">
+                        <p>Partner Sales</p>
+                        <p className="sale-swiggy-sale">Swiggy Sale - {props.swiggy_sale}</p>
+                        <p className="sale-zomato-sale">Zomato Sale - {props.zomato_sale}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/components/Sales.js
+++ src/components/Sales.js
@@ -6,26 +6,24 @@ const supabase = createClient('https://gweqbzdtgeuwhjoyknnc.supabase.co', 'eyJhb
 
 const Sales = () => {
 
-    const [data, setData] = useState([]);
-    const [data2, setData2] = useState([]);
+    const [databtm, setDataBtm] = useState([]);
+    const [dataec2, setDataEc2] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
-          const { data, error } = await supabase.rpc('fetch_data', {});
-          if (error) {
-            console.error('Error fetching data:', error.message);
-          } else {
-            
-            setData(data.table1);
-            setData2(data.table2);
-            console.log(data.table1)
-            console.log(data.table2)
-          }
+            const { data, error } = await supabase.rpc('fetch_data', {});
+            if (error) {
+                console.error('Error fetching data:', error.message);
+            } else {
+
+                setDataBtm(data.table1);
+                setDataEc2(data.table2);
+            }
         };
 
-    
+
         fetchData();
-      }, []);
+    }, []);
 
     return (
         <div className="home-container">
@@ -40,13 +38,25 @@ const Sales = () => {
 
 
                 <div className="sales-body-container">
-                    {data.map((row) => (
-                        <SaleFiguresBox key={row.id} shop_name={row.shop_name} />
-                    ))}
-
-                    {data2.map((row) => (
-                        <SaleFiguresBox key={row.id} shop_name={row.shop_name} />
-                    ))}
+                    <div className="sales-box">
+                        <p>BTM</p>
+                        {databtm.map((row) => (
+                            <SaleFiguresBox key={row.id} shop_name={row.shop_name} date={row.date} cash_sale={row.cash_sale} card_sale={row.card_sale} upi_sale={row.upi_sale} swiggy_sale={row.swiggy_sale} zomato_sale={row.zomato_sale} />
+                        ))}
+                    </div>
+                    <div className="sales-box">
+                    <p>EC2</p>
+                        {dataec2.map((row) => (
+                            <SaleFiguresBox key={row.id} shop_name={row.shop_name} date={row.date} cash_sale={row.cash_sale} card_sale={row.card_sale} upi_sale={row.upi_sale} swiggy_sale={row.swiggy_sale} zomato_sale={row.zomato_sale} />
+                        ))}
+                    </div>
+                    <div className="sales-box"></div>
+                    <div className="sales-box"></div>
+                    <div className="sales-box"></div>
+                    <div className="sales-box"></div>
+                    <div className="sales-box"></div>
+                    <div className="sales-box"></div>
+                    <div className="sales-box"></div>
                 </div>
             </div>
         </div>
@@ -54,3 +64,35 @@ const Sales = () => {
 }
 
 export default Sales;
+
+/*
+
+const [data, setData] = useState([]);
+    //const [data2, setData2] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data, error } = await supabase.rpc('fetch_data', {});
+            if (error) {
+                console.error('Error fetching data:', error.message);
+            } else {
+
+                setData(data.table1);
+                //setData2(data.table2);
+            }
+        };
+
+
+        fetchData();
+    }, []);
+
+
+
+                    {data.map((row) => (
+                        <SaleFiguresBox key={row.id} shop_name={row.shop_name} date={row.date} cash_sale={row.cash_sale} card_sale={row.card_sale} upi_sale={row.upi_sale} swiggy_sale={row.swiggy_sale} zomato_sale={row.zomato_sale}/>
+                    ))} 
+
+                                        {/*data2.map((row) => (
+                        <SaleFiguresBox key={row.id} shop_name={row.shop_name} />
+                    ))}
+*/
